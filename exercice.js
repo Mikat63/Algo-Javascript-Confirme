5f3e9e69591b4f512393f7f7420b75bb2686baed
mikaT <m.tores63@outlook.fr>
2025-10-15 15:03:52 +0200

    completed: exercice 3 and 4

--- exercice.js
+++ exercice.js
@@ -1,22 +1,54 @@
-// Exercice 1 : Boucle for : 
+// Exercice 1 : Boucle for :
 let pyramidStars = "";
 
-for(let i = 0; i < 5; i++) {
-    pyramidStars += "*"
-    console.log(pyramidStars)
+for (let i = 0; i < 5; i++) {
+  pyramidStars += "*";
+  console.log(pyramidStars);
 }
 // Exercice 2 : Boucle foreach :
-let arraySuperHeros = ["Vénom", "Punisher", "Hulk", "Deadpool", "Spider-Man", "Wolverine"];
+let arraySuperHeros = [
+  "Vénom",
+  "Punisher",
+  "Hulk",
+  "Deadpool",
+  "Spider-Man",
+  "Wolverine",
+];
 const sentence = "je suis fan de ";
 
-arraySuperHeros.forEach(element => {
-    let result = sentence + element
-    console.log(result)
+arraySuperHeros.forEach((element) => {
+  let result = sentence + element;
+  console.log(result);
 });
 
 // Exercice 3 : Boucle while :
+let countDown = 10;
+
+while (countDown > 0) {
+  console.log(countDown);
+  countDown--;
+
+  if (countDown === 0) {
+    countDown = "Décollage !";
+  }
+}
+
+console.log(countDown);
 
 // Exercice 4 : Boucles combinées :
+let arrayFriends = ["Hugo", "Jonathan", "Kevin", "Mathieu", "William"];
+
+for (let i = 0; i < arrayFriends.length; i++) {
+  const prenom = arrayFriends[i];
+  let j = 0;
+  let ligne = "";
+  while (j < i + 1) {
+    ligne += prenom + " ";
+    j++;
+  }
+  console.log(ligne);
+}
+
 // Exercice 5.1 :
 // Exercice 5.2 :
 // Exercice 5.3 :
