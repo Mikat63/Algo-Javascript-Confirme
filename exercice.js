f3d43d34346f8bc92df30ccb0f719adc9bcbd4ef
mikaT <m.tores63@outlook.fr>
2025-10-15 14:20:20 +0200

    add : file created

---
+++ exercice.js
@@ -0,0 +1,15 @@
+// Exercice 1 : Boucle for : 
+// Exercice 2 : Boucle foreach :
+// Exercice 3 : Boucle while :
+// Exercice 4 : Boucles combinées :
+// Exercice 5.1 :
+// Exercice 5.2 :
+// Exercice 5.3 :
+// Exercice 5.4 :
+// Exercice 5.5 :
+// Exercice 5.6 :
+// Exercice 5.7 :
+// Exercice 5.8 :
+// Exercice 5.9 :
+// Exercice 5.10 :
+// Exercice 5.11 :
